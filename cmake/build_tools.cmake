919933d16a2d5c03f6ba70d7ffa35d32ab04b00c
seanchann <seanchann@foxmail.com>
2017-01-15 18:49:41 +0800

    1. autogenerate build.h header. 2. add build tools cmake file

---
+++ cmake/build_tools.cmake
@@ -0,0 +1,7 @@
+macro(BuildTools)
+  set(BUILD_HEADER_OUT_FILE "${CMAKE_CURRENT_SOURCE_DIR}/include/asterisk/build.h")
+  add_custom_command(OUTPUT ${BUILD_HEADER_OUT_FILE}
+   COMMAND /bin/sh "${CMAKE_CURRENT_SOURCE_DIR}/build_tools/make_build_h" > ${BUILD_HEADER_OUT_FILE})
+  add_custom_target(buildHeader ALL DEPENDS ${BUILD_HEADER_OUT_FILE})
+
+endmacro()
